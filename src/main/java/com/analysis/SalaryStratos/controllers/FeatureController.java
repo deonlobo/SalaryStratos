8eaedbc15582e3e3c74a05e53c6c4f20e98aed05
GaganSingh5 <gagansingh0797@gmail.com>
2023-11-27 22:32:50 -0500

    Added Scraper Code

--- src/main/java/com/analysis/SalaryStratos/controllers/FeatureController.java
+++ src/main/java/com/analysis/SalaryStratos/controllers/FeatureController.java
@@ -6,14 +6,18 @@ import com.analysis.SalaryStratos.models.Job;
 import com.analysis.SalaryStratos.models.JobValidation;
 import com.analysis.SalaryStratos.features.DataValidation;
 import com.analysis.SalaryStratos.features.FetchAndUpdateData;
+import com.analysis.SalaryStratos.models.SuggestionModel;
+import com.analysis.SalaryStratos.models.WordSuggestionResponse;
+import com.analysis.SalaryStratos.services.JobDataTrie;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class FeatureController {
     @Autowired
     private final FetchAndUpdateData jobService;
@@ -24,18 +28,64 @@ public class FeatureController {
     @Autowired
     private final SearchFrequency searchFrequency;
 
-    public FeatureController(FetchAndUpdateData jobService, DataValidation dataValidation, JobSearchAndSort jobSearchAndSort, SearchFrequency searchFrequency) {
+    @Autowired
+    private final JobDataTrie jobData;
+
+    public FeatureController(FetchAndUpdateData jobService, DataValidation dataValidation, JobSearchAndSort jobSearchAndSort, SearchFrequency searchFrequency, JobDataTrie jobData) {
         this.jobService = jobService;
         this.dataValidation = dataValidation;
         this.jobSearchAndSort = jobSearchAndSort;
         this.searchFrequency = searchFrequency;
+        this.jobData = jobData;
     }
 
-    @RequestMapping(value = "")
+    @CrossOrigin
+    @PostMapping(value = "/correctWords")
     @ResponseBody
-    public String index() {
-        return "helloooo world !!";
+    public String[] getCorrectWord(@RequestBody String  request) {
+        System.out.println(request);
+        return new String[]{"abc", "123"};
     }
+
+    @CrossOrigin
+    @GetMapping(value = "/recentSearches")
+    @ResponseBody
+    public String[] getRecentSearches() {
+
+        return new String[]{"abc", "123"};
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/wordSuggestions")
+    @ResponseBody
+    public WordSuggestionResponse getSuggestions(@RequestBody String searchTerm) {
+        List<String> validatedSearchTerms = DataValidation.validateSuggestionRequest(searchTerm);
+        WordSuggestionResponse suggestons = new WordSuggestionResponse();
+
+        if (validatedSearchTerms.isEmpty()) {
+            System.out.println("IsEmpty");
+            suggestons.setResponseValid(false);
+        } else {
+            List<SuggestionModel> suggestedWordsList = new ArrayList<>();
+            for (String searchPrefix: validatedSearchTerms) {
+                System.out.println(searchPrefix);
+                SuggestionModel model = new SuggestionModel();
+                model.setWord(searchPrefix);
+
+                model.setSuggestedWords(jobData.suggestWordsBasedOnPrefix(searchPrefix));
+
+                suggestedWordsList.add(model);
+            }
+
+            suggestons.setWordSuggestions(suggestedWordsList);
+        }
+
+
+
+        return suggestons;
+    }
+
+
 
     @RequestMapping(value = "/jobs")
     @ResponseBody
